2d4b65f851b01b4240d0b64e0a1e7566188b93fa
Joe Liu <joeliu0999@gmail.com>
2024-05-26 14:22:44 +0800

    coinCollect animation, start scene, adjust positioning of obstacle,etc

---
+++ assets/script/coinFloor.ts
@@ -0,0 +1,40 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    private coinAnimation: cc.Animation = null;
+    
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        this.coinAnimation = this.node.getComponent(cc.Animation);
+    }
+
+    start () {
+
+    }
+
+    onBeginContact(contact,self,other){
+        if(other.node.name === "Mario"){
+            this.node.getComponent(cc.Animation).play("collectCoin");
+            //call backfunction (no parenthses) need bind else "this" will refers to the window
+            this.coinAnimation.on("finished",this.removeSprite.bind(this));
+        }
+    }
+    //status will be auto pass in by call back
+    private removeSprite(status){
+        if(status==="finished"){
+            this.node.destroy();
+        }
+    }
+
+    // update (dt) {}
+}
